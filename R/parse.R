9abe8a8e3e919412bac44edb58e5979ff2bc38cf
François Michonneau <francois.michonneau@gmail.com>
2020-06-12 16:31:43 +0200

    convert username/repo into full glossary URL

--- R/parse.R
+++ R/parse.R
@@ -6,11 +6,29 @@ langs <- c("en", "fr", "es")
 ##'
 ##' Retrive a YAML-formatted glossary from the web or locally.
 ##'
-##' The specification of the file is available from https://github.com/gvwilson/glossary/#readme
-##' @param url the path for the glossary
+##' The specification of the file is available from
+##' https://github.com/gvwilson/glossary/#readme
+##'
+##' @param url the url, path for the glossary or the OWNER/REPO for a GitHub
+##'   repository that hosts a glossary.
+##'
+##' @details If <OWNER/REPO> is provided to the `url` argument, the successful
+##'   conversion into an URL that will resolve to a glossary assumes that the
+##'   glossary is called `glossary.yml` in the repository and it lives in the
+##'   branch `master`.
+##'
 ##' @return a list
 ##' @export
 get_glossary <- function(url = "https://raw.githubusercontent.com/gvwilson/glossary/master/glossary.yml", cache = tempdir()) {
+
+  if (identical(nchar(gsub("[^/]", "", url)), 1L) &&
+        !grepl("\\.yml$", url, ignore.case = TRUE)) {
+    url <- paste0(
+      "https://raw.githubusercontent.com/",
+      url,
+      "/master/glossary.yml"
+    )
+  }
 
   Glossary$new(url, cache_path = cache)
 
