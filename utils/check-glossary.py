6ec9cf1aa74d68f082332596e2ab1ec53a7bf7ef
TomKellyGenetics <tomkellygenetics@gmail.com>
2021-04-09 16:23:27 +0900

    resolve conflicts in utils/check-glossary.py

--- utils/check-glossary.py
+++ utils/check-glossary.py
@@ -30,7 +30,7 @@ from collections import Counter
 # Keys for entries and definitions.
 ENTRY_REQUIRED_KEYS = {'slug'}
 ENTRY_OPTIONAL_KEYS = {'ref'}
-ENTRY_LANGUAGE_KEYS = {'af', 'am', 'ar', 'de', 'en', 'es', 'fr', 'he', 'ja', 'nl', 'pt', 'zu'}
+ENTRY_LANGUAGE_KEYS = {'af', 'am', 'ar', 'de', 'en', 'es', 'fr', 'he',  'ko', 'ja', 'nl', 'pt', 'zu'}
 ENTRY_KEYS = ENTRY_REQUIRED_KEYS | \
              ENTRY_OPTIONAL_KEYS | \
              ENTRY_LANGUAGE_KEYS
